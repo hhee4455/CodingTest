42d29afa11a42c0f5d6178d9266561b6619da4c6
이승준 <127376832+hhee4455@users.noreply.github.com>
2025-02-22 13:25:06 +0900

    [level 2] Title: 타겟 넘버, Time: 374.40 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 2.97 ms
+메모리: 10.1 MB, 시간: 374.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 24일 12:15:22
+2025년 02월 22일 13:25:05
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,16 @@
 def solution(numbers, target):
-    def dfs(idx, total):
-        if idx == len(numbers):
-            if total == target:
-                return 1
-            else:
-                return 0
-        else:
-            return dfs(idx + 1, total + numbers[idx]) + dfs(idx + 1, total - numbers[idx])
-
-    answer = dfs(0, 0)
+    answer = 0
+    
+    def dfs(i,current_sum):
+        nonlocal answer
+        
+        if i == len(numbers):
+            if current_sum == target:
+                answer += 1
+            return
+        
+        dfs(i+1,current_sum + numbers[i])
+        dfs(i+1,current_sum - numbers[i])
+    
+    dfs(0,0)
     return answer
